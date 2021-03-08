25e6b4792693562c884fd8c930333e05dad2abf3
Rachmad_Sukri <rachmadsukri@student.telkomuniversity.ac.id>
2021-03-08 12:19:58 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if4025/galerihewan/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if4025.galerihewan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
