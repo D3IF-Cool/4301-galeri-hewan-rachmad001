62681a9ff9352e10244a995f6c80da575898aa95
Rachmad_Sukri <rachmadsukri@student.telkomuniversity.ac.id>
2021-05-03 16:33:34 +0700

    Refactor fitur main ke fragment

--- app/src/main/java/org/d3if4025/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4025/galerihewan/MainActivity.kt
@@ -2,39 +2,15 @@ package org.d3if4025.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import org.d3if4025.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        Log.d("MainActivity", "Jumlah data: " + getData().size )
-        with(binding.recyclerView) {
-            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
-            adapter = MainAdapter(getData())
-            setHasFixedSize(true)
-        }
-    }
-
-    private fun getData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi)
-            )
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/org/d3if4025/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4025/galerihewan/ui/main/MainAdapter.kt
@@ -1,9 +1,9 @@
-package org.d3if4025.galerihewan
+package org.d3if4025.galerihewan.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.d3if4025.galerihewan.databinding.ActivityMainBinding
+import org.d3if4025.galerihewan.model.Hewan
 import org.d3if4025.galerihewan.databinding.ListItemBinding
 
 class MainAdapter(private val data: List<Hewan>) :
